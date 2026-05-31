de2fc020a7ca35222e623934681f0d342974e1df
Santyjl44 <santyjl44@gmail.com>
2026-05-30 20:04:12 -0600

    se añadio el estilo de titulo a la funcion de titulo

--- por_siempre_raul_y_tatiana/components/texto.py
+++ por_siempre_raul_y_tatiana/components/texto.py
@@ -1,5 +1,5 @@
 import reflex as rx
-from por_siempre_raul_y_tatiana.styles import texto_style
+from por_siempre_raul_y_tatiana.styles import texto_style , titulo_style
 
 def texto_plantilla(texto:str) -> rx.Component:
     return rx.text(
