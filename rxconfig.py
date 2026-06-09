4e922f406566b0bd386ec73fa438d3159d790a12
Santyjl44 <santyjl44@gmail.com>
2026-06-09 17:25:17 -0600

    se quito el boton de hecho con reflex

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="por_siempre_raul_y_tatiana",
+    show_built_with_reflex=False,
     plugins=[
         rx.plugins.sitemap.SitemapPlugin,
         rx.plugins.RadixThemesPlugin
