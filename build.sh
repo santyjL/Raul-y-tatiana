1e624347619d3790e6f5330873f848577f696b3f
Santyjl44 <santyjl44@gmail.com>
2026-06-09 17:19:01 -0600

    se añadio el public al gitignore y se actualizo el build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=https://raul-y-tatiana-production.up.railway.app
-reflex export --frontend-only
+REFLEX_API_URL=https://raul-y-tatiana-production.up.railway.app reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
